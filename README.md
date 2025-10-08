96d7a6e44fb4599e9c303f75862e43c27918513c
Ryan Bride <bride.r@northeastern.edu>
2025-10-08 18:28:33 -0400

    Finished up the Prototype Matrix Rom file

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ RTL design for a "5G" multigigabit QC-LDPC pipelined encoder in System Verilog.
 Implements code length of 1296, at a rate of 5/6, and uses the RU Algorithm in conjunction with various other optimizations derived from research publications to create an high throughput QC-LDPC Encoder. 
 
 The goal of this project was to put one together as quickly (yet still accurate) as possible. 
+
+
+
+This Project includes .mem files which define the base parity matrix whose values define the shifts of the ZxZ identity matrix used to encode data. Each value is stored as a hex, and the - values present in the defined matrix found in the standard are replaced with the highest value of what would be the memory width required to store the matrix entires in memory. 
--- src/ProtoMatrixRom.sv
+++ src/ProtoMatrixRom.sv
@@ -1,3 +1,5 @@
+`timescale 1ns / 1ps
+`default_nettype none
 // -------------------------------------------------------------------------
 // LUT Based Memory Module, Has the benefits of Being Asynchronous and fast
 // Given Z we can determine the total Codelength as the 5/6 Rate Parity Matrix Has 
@@ -7,40 +9,32 @@
 module ProtoMatrixRom #(
     parameter int Z = 54,                   
     localparam int CODEWORD_LEN = Z * 24,
+    localparam int WIDTH = $clog2(Z), //Width needed to store values from 0 to Z-1
     localparam int DEPTH = 24*4,   //The depth would differ if the rate changed 
-    localparam int ADDRW = $clog2(Z) 
+    localparam int ADDRW = $clog2(DEPTH)     
 )(
     input wire logic [ADDRW-1:0] addr,
     output     logic [CODEWORD_LEN-1:0] data
 ); 
 
-//Since the Input matrix stores either a value from 0 to Z-1 or a - for a zero block
-//we can store the - as the max value of the Width since none of the Z's have a value
-//greater than the 2^ADDRW
-
-
-
-
-
-
-module rom_async #(
-    parameter WIDTH=8, 
-    parameter DEPTH=256, 
-    parameter INIT_F="",
-    localparam ADDRW=$clog2(DEPTH)
-    ) (
-    input wire logic [ADDRW-1:0] addr,
-    output     logic [WIDTH-1:0] data
-    );
-
-    logic [WIDTH-1:0] memory [DEPTH];
-
+    logic [WIDTH-1:0] memory [DEPTH-1:0];
+    // Initialize memory from file
     initial begin
-        if (INIT_F != 0) begin
-            $display("Creating rom_async from init file '%s'.", INIT_F);
-            $readmemh(INIT_F, memory);
+        if (Z == 27) begin
+            $readmemh("ProtoMatrixRom_27.mem", memory);
         end
-    end
 
-    always_comb data = memory[addr];
+        else if (Z == 54) begin
+            $readmemh("ProtoMatrixRom_54.mem", memory);
+        
+        end else if (Z == 81) begin
+            $readmemh("ProtoMatrixRom_81.mem", memory);
+        
+        end else begin : assert_invalid_cfg
+            $fatal( 1, "Invalid Configuration detected, Unsupported Z value: %0d. Supported values are 27, 54, and 81. - aborting", Z);
+        end
+
+
+    assign data = memory[addr];
+
 endmodule
