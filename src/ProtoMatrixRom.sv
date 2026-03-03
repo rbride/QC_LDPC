9c116c9dd90b170f7aa82cb25fb5508f48201b47
Ryan Bride <bride.r@northeastern.edu>
2026-03-03 18:15:16 -0500

    Updtaed ProtoMatrixRom

--- src/ProtoMatrixRom.sv
+++ src/ProtoMatrixRom.sv
@@ -1,9 +1,4 @@
 `timescale 1ns / 1ps
-// -------------------------------------------------------------------------
-//
-//  All defined Roms Support the included standard 5/6 rate with the 3 standard Z values of 27,54,81
-//
-// ------------------------------------------------------------------------
 //For yosys (* ram_style = "block" *) selects block RAM
 //for yosys (* ram_style = "distributed" *) selects LUT RAM
 //For Vivado(* rom_style = "block" *) reg [data_size-1:0] myrom [2**addr_size-1:0];
@@ -11,77 +6,92 @@
 //infers LUT(* rom_style = "distributed" *) reg [data_size-1:0] myrom [2**addr_size-1:0]; Vivado
 //infers block (* rom_style = "block" *) reg [data_size-1:0] myrom [2**addr_size-1:0]; Vivado 
 // Quartus: // synthesis ramstyle = "logic" or "lcell_ram"   Forces LUT based
-/* Note: "-" (zero block/skip) values are stored as the maximum value available for the 
-    given WIDTH as none of the possible Z values are equal to The maximum value of the WIDTH   */
-// ------------------------------------------------------------------------
-
-//# TODO I have to fix the entries for 27, as they need to be the highest value of the width of MAXZ 
-module ProtoMatrixRom_SingleLUT #(
-    parameter int THE_Z = 54,                   
-    parameter int WIDTH = $clog2(THE_Z), // 6
-    parameter int DEPTH = 96,   
-    parameter int ADDRW = 7,    //Clog2(Depth)
-    parameter int NUM_PARITY_BLKS = 4,
-    parameter int P_LVL = 1
-)(
-    input wire logic [ADDRW-1:0] addr,
-    output logic        [WIDTH-1:0] data_out [0:NUM_PARITY_BLKS-1]
-); 
+// ==========================================================================
+// Proto Matrix LUT Distributed ROM
+// Author: Ryan Bride
+// Description: 
+//      The Module belows provides a manner to load in a nd store the shift 
+//      Values in rom, as an input the module takes an address, and by default
+//      outputs 4 (or the total number of generated parity blks) shift values
+//      the module also outputs a skip bit as well that is stored in the MSB
+//      of the output.
+// 
+//      To use this module for a different system:
+//          1. Set parameters to match your proto matrix dimensions
+//          2. Replace $readmemh calls with your shift value files
+//             File format: one hex value per line
+//             Each value is (NUM_PBLKS * WIDTH) bits wide
+//             parity lane 0 in LSBs, lane N in MSBs
+//          3. Update Z_VALUES to match your supported Z set
+//
+//      MEM FILE FORMAT per entry:
+//          The memory File should be formated in the same way it is listed 
+//          in the IEEE standard for the 801.11, so for instance 4 rows of 20 
+//          finally any sentinal value or skip value or whatever (-1) should be 
+//          stored as the highest value possible in $Clog2(Z)
+// ==========================================================================
+module ProtoMatrixRom #(
+    parameter int MAXZ                    = 81,
+    parameter int NUM_SUP_Z               = 2,
+    parameter int NUM_PBLKS               = 4,   //The top module defines one rotator per PBLK ROW 
+    parameter int NUM_IBLKS               = 20,  //Also equal to the amount of data messages sen 
+    parameter int Z_VALS [0:NUM_SUP_Z-1]  = {27,81},
     
-    (* ram_style = "distributed" *) logic [WIDTH-1:0] memory [0:DEPTH-1];
-
-    initial begin
-        if (THE_Z == 27) begin
-            $readmemh("648B_5_6_ProtoMat.mem", memory);
-        end else if (THE_Z == 54) begin
-            $readmemh("1296B_5_6_ProtoMat.mem", memory);
-        end else if (THE_Z == 81) begin
-            $readmemh("1944B_5_6_ProtoMat.mem", memory);
-        end else begin : assert_invalid_cfg
-            $fatal( 1, "Invalid Configuration detected, Unsupported Z value: %0d. Supported values are 27, 54, 81\n\
-                    f you need a different Z value, ensure that a file is provided and change the code In \n\
-                    ProtoMatrixRom.sv to load the .mem file values for your specific Z - aborting", THE_Z);
-        end
-    end
-
-    always_comb begin
-        for(int i=0; i<NUM_PARITY_BLKS; i++) begin
-            data_out[i] = memory[addr +(i*(DEPTH/NUM_PARITY_BLKS)-1)];
-        end
-    end
-endmodule
-
-// ------------------------------------------------------------------------- 
-// -------------------------------------------------------------------------
-// -------------------------------------------------------------------------
-module ProtoMatrixRom_MultiLUT #(
-    parameter int NUM_Z             =   3,
-    parameter int Z_VALUES[NUM_Z]   =   {27, 54, 81},
-    parameter int DEPTH             =   288,  
-    parameter int WIDTH             =   7,
-    parameter int ADDRW             =   9,
-    parameter int NUM_PARITY_BLKS   =   4,
-
+    localparam int WIDTH                  = $clog2(MAXZ),
+    localparam int DEPTH                  = NUM_IBLKS*NUM_PBLKS,
+    localparam int ADDRW                  = $clog2(DEPTH),
+    localparam int WORD_WIDTH             = NUM_PBLKS*WIDTH
 )(
-    input wire logic [ADDRW-1:0] addr,
-    output logic          [WIDTH-1:0] data_out [0:NUM_PARITY_BLKS-1]
-
+    input  logic [ADDRW-1:0] addr,
+    output logic [WIDTH:0]   data_out [0:NUM_PBLKS-1]   //Not minus one so there is room for the skip bit [msb = skip bit]
 );
- 
-    (* ram_style = "distributed" *) logic [WIDTH-1:0] memory [0:DEPTH-1];    
-    initial begin
-        readmemh("27B_5_6_ProtoMat.mem", memory, 0*(DEPTH/NUM_Z), 1*(DEPTH/NUM_Z)-1);
-        //readmemh("54B_5_6_ProtoMat.mem", memory, 1*(DEPTH/NUM_Z), 2*(DEPTH/NUM_Z)-1);
-        readmemh("81B_5_6_ProtoMat.mem", memory, 2*(DEPTH/NUM_Z), 3*(DEPTH/NUM_Z)-1);
-    end
-    
-    //Note the order for the Req_Z is the last Z value defined in the Z_VAl Array is corresponds to the most sig bit of Req_z
-    //Determine in the encoder logic send the address offset with the requested z don't handle it in memory 
-    always_comb begin
-        for(int i=0; i<NUM_PARITY_BLKS; i++) begin
-            data_out[i] = memory[addr+(i*(DEPTH/NUM_PARITY_BLKS)-1)];
+    (* ram_style = "distributed" *) logic [WIDTH:0] mem [0:DEPTH*NUM_SUP_Z-1];
+    logic [WIDTH-1:0] shift_vals [0:NUM_PBLKS-1];
+    logic             skip_vals  [0:NUM_PBLKS-1];
+
+    //Populate the ROM
+    genvar zi;
+    generate
+        for(zi=0; zi< NUM_SUP_Z; zi++) begin : ROM_INIT_SYN
+            localparam int OFFSET   = zi * DEPTH;
+            localparam int SENTINEL = (1<<$clog2(Z_VALS[zi]))-1;
+            
+            initial begin
+                //Load in the Desired memory block
+                if          (Z_VALUES[zi] == 27) begin
+                    $readmemh("z27_protomat.mem", mem, OFFSET, (OFFSET+DEPTH-1));
+                end else if (Z_VALUES[zi] == 54) begin
+                    $readmemh("z54_protomat.mem", mem, OFFSET, (OFFSET+DEPTH-1));
+                end else if (Z_VALUES[zi] == 81) begin
+                    $readmemh("z81_protomat.mem", mem, OFFSET, (OFFSET+DEPTH-1));
+                end
+
+                //Populate Skip Values, scan every lane and put the msb to one if its a skip
+                for(int mem_entry=OFFSET; mem_entry<(OFFSET+DEPTH); mem_entry++) begin
+                    automatic logic [WIDTH:0] raw_val;
+                    raw_val = mem[mem_entry];
+
+                    if(raw_val == WIDTH'(SENTINEL)) begin
+                        //Set the skip bit and clear the rest 
+                        mem[mem_entry][WIDTH]     = 1'b1;   
+                        mem[mem_entry][WIDTH-1:0] = '0;
+                    end else begin
+                        mem[mem_entry][WIDTH]     = 1'b0;
+                    end
+                end
+            end
         end
-    end
+    endgenerate
+
+    //Generate Outputs/combine output lanes corresponding to the address
+    genvar lane
+    generate
+        for(lane=0; lane<NUM_PBLKS; lane++) begin
+            assign data_out[lane][WIDTH-1:0] = mem[addr+(lane*(DEPTH/NUM_PBLKS)-1)][WIDTH-1:0];
+            assign data_out[lane][WIDTH]     = mem[addr+(lane*(DEPTH/NUM_PBLKS)-1)][WIDTH];
+        end
+    endgenerate
+
 endmodule
 
 //BRAM Version of ram
@@ -115,15 +125,4 @@ module ProtoMatrixRom_BRAM #(
         end
     end
     
-
 endmodule
-
-/*
-    string filenames[3];
-    initial begin
-        foreach (Z_VALUES[i]) begin
-            filenames[i] = {sformatf("%0d", Z_VALUES[i]), "B_5_6_ProtoMat.mem"};
-            readmemh(filenames[i], ram, (i*(DEPTH/3)), (((i+1)*(DEPTH/3))-1));
-        end 
-    end
-*/
